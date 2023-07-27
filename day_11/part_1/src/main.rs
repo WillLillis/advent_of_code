c281fb923ecf1b16e6f1ab9d3c4ecc6c4e617d0e
WillLillis <wlillis@umass.edu>
2023-07-27 13:17:46 -0400

    Started day 11

---
+++ day_11/part_1/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+pub enum MonkeyOp {
+    y(i32),
+    add_by(i32)
+}
+
+pub struct Monkey {
+    test_num: i32,
+    true_dest: usize,
+    false_dest: usize
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
