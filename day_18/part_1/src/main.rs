4f57abef13554b6305bca864179897f9234f1b6a
WillLillis <wlillis@umass.edu>
2023-08-07 18:20:31 -0400

    Day 18 part 1 done, stuck on part 2

--- day_18/part_1/src/main.rs
+++ day_18/part_1/src/main.rs
@@ -1,13 +1,87 @@
-// A quick sketch of how I'd like to solve this, just some initial thoughts
-// 
-// - There are 6 directions to examine the shape from to get its surface area,
-// but because of symmetry, we only need to examine 3 of them
-// - We can use a hash map for each viewing angle, storing the relevent 2
-// coordinates and ignoring the third, as repeated inserts won't increment our count
-//
-// - The three coordinate pairs will be (x,z), (y,z), and (x,y)
+use std::fs;
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct XYZCoor<T> {
+    x: T,
+    y: T,
+    z: T
+}
+
+impl<T> XYZCoor<T> {
+    fn new(x: T, y: T, z: T) -> Self {
+        XYZCoor {
+            x,
+            y,
+            z
+        }
+    }
+}
 
 fn main() {
-    // TODO: parse input file into (x,y,z) tuples
-    // implement the counting algo defined above
+    let input = fs::read_to_string("input.txt")
+        .expect("Failed to read input file");
+
+    let mut points: Vec<XYZCoor<i32>> = Vec::new();
+
+    for line in input.lines() {
+        let nums: Vec<i32> = line.trim().split(&[' ', ','][..])
+            .filter_map(|x| x.parse::<i32>().ok())
+            .collect();
+
+        assert!(nums.len() == 3);
+        points.push(XYZCoor::new(nums[0], nums[1], nums[2]));
+    }
+
+    // shouldn't completely throw out old idea of (x,z), (y,z), and (x,y)
+
+    let mut table: HashSet<XYZCoor<i32>> = HashSet::new();
+
+    for entry in &points {
+        table.insert(entry.clone()); 
+    }
+
+    // now count up the surface area
+    let mut surface_area: u32 = 0;
+    let mut test_point: XYZCoor<i32> = XYZCoor::new(0, 0, 0);
+    for entry in &points {
+        test_point.x = entry.x;
+        test_point.y = entry.y;
+        test_point.z = entry.z;
+
+        // +/- x
+        test_point.x -= 1;
+        if !table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.x += 2;
+        if !table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.x -= 1;
+
+        // +/- y
+        test_point.y -= 1;
+        if !table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.y += 2;
+        if !table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.y -= 1;
+        
+        // +/- z
+        test_point.z -= 1;
+        if !table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.z += 2;
+        if !table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.z -= 1;
+    }
+        
+    println!("Approximate surface area: {}", surface_area);
 }
--- day_18/part_2/src/main.rs
+++ day_18/part_2/src/main.rs
@@ -1,3 +1,86 @@
+use std::fs;
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+struct XYZCoor<T> {
+    x: T,
+    y: T,
+    z: T
+}
+
+impl<T> XYZCoor<T> {
+    fn new(x: T, y: T, z: T) -> Self {
+        XYZCoor {
+            x,
+            y,
+            z
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("test_input.txt")
+        .expect("Failed to read input file");
+
+    let mut points: Vec<XYZCoor<i32>> = Vec::new();
+
+    for line in input.lines() {
+        let nums: Vec<i32> = line.trim().split(&[' ', ','][..])
+            .filter_map(|x| x.parse::<i32>().ok())
+            .collect();
+
+        assert!(nums.len() == 3);
+        points.push(XYZCoor::new(nums[0], nums[1], nums[2]));
+    }
+
+    let mut lava_table: HashSet<XYZCoor<i32>> = HashSet::new();
+
+    for entry in &points {
+        lava_table.insert(entry.clone()); 
+    }
+
+    // how to detect/ count interior points????
+    // now count up the surface area
+    let mut surface_area: u32 = 0;
+    let mut test_point: XYZCoor<i32> = XYZCoor::new(0, 0, 0);
+    for entry in &points {
+        test_point.x = entry.x;
+        test_point.y = entry.y;
+        test_point.z = entry.z;
+
+        // +/- x
+        test_point.x -= 1;
+        if !lava_table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.x += 2;
+        if !lava_table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.x -= 1;
+
+        // +/- y
+        test_point.y -= 1;
+        if !lava_table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.y += 2;
+        if !lava_table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.y -= 1;
+        
+        // +/- z
+        test_point.z -= 1;
+        if !lava_table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.z += 2;
+        if !lava_table.contains(&test_point) {
+            surface_area += 1;
+        }
+        test_point.z -= 1;
+    }
+        
+    println!("Approximate surface area: {}", surface_area);
 }
