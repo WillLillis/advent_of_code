30e74283b20e2fd2c39d9a21324a066c1ee882ee
WillLillis <wlillis@umass.edu>
2023-08-13 14:44:32 -0400

    Checkpoint on day 22 part 1

--- day_22/part_1/src/main.rs
+++ day_22/part_1/src/main.rs
@@ -1,5 +1,60 @@
 use std::{fs, cmp};
 
+enum Rotation {
+    Clockwise,
+    CounterClockwise
+}
+
+enum Move {
+    Move(usize),
+    Turn(Rotation)
+
+}
+
+enum Facing {
+    Right, // >
+    Down, // v
+    Left, // <
+    Up // ^
+}
+
+impl Facing {
+    fn turn(&mut self, way: Rotation) {
+        match way {
+            Rotation::Clockwise => {
+                Facing::turn_clockwise(self);
+            },
+            Rotation::CounterClockwise => {
+                Facing::turn_counterclockwise(self);
+            }
+        }
+    }
+
+    fn turn_clockwise(&mut self) {
+        *self = match self {
+            Facing::Right => Facing::Down,
+            Facing::Down => Facing::Left,
+            Facing::Left => Facing::Up,
+            Facing::Up => Facing::Right
+        };
+    }
+
+    fn turn_counterclockwise(&mut self) {
+        *self = match self {
+            Facing::Right => Facing::Up,
+            Facing::Down => Facing::Right,
+            Facing::Left => Facing::Down,
+            Facing::Up => Facing::Left
+        };
+    }
+}
+
+struct Pos {
+   orientation: Facing,
+   row: usize,
+   row_idx: usize
+}
+
 fn get_notes(file_name: &str) -> (Vec<Vec<char>>, String) {
     let input = fs::read_to_string(file_name)
         .expect("Failed to read the input file");
@@ -38,6 +93,72 @@ fn get_notes(file_name: &str) -> (Vec<Vec<char>>, String) {
     println!("\n\n\n{}", path);
 
     return (map, path);
+}
+
+// find the first valid index in a given row
+fn row_first_idx(map: &Vec<Vec<char>>, row: usize, from_left: bool) -> usize {
+
+
+    0
+}
+
+fn col_first_idx(map: &Vec<Vec<char>>, col: usize, from_top: bool) -> usize {
+
+
+    0
+}
+
+fn make_move(map: &Vec<Vec<char>>, mut pos: Pos, next_move: Move) -> Pos {
+    match next_move {
+        Move::Move(num) => {
+            match pos.orientation {
+                Facing::Right => {
+                    let curr_row = pos.row;
+                    let mut curr_idx = pos.row_idx;
+                    for incr in 1..=num {
+                        // first check if we're running off the right edge
+                        if curr_idx + incr >= map[curr_row].len() {
+                            // if we can wrap around do it
+                            
+                            // otherwise we're done
+                            
+                            break;
+                        }
+                        // next check if we're blocked
+                        if map[curr_row][curr_idx + incr] == '#' {
+                            pos.row_idx = curr_idx + incr;
+                            break;
+                        } else if map[curr_row][curr_idx + incr] == ' ' { // or if we can wrap around
+                        
+                        } else if map[curr_row][curr_idx + incr] == ',' { // or if we can just move one
+
+                        } else {
+                            panic!("Unexpected move!");
+                        }
+                    }
+                    
+                },
+                Facing::Down => {
+
+                    
+                },
+                Facing::Left => {
+
+                    
+                },
+                Facing::Up => {
+                    
+                }
+            }
+
+            pos
+        },
+        Move::Turn(dir) => {
+            pos.orientation.turn(dir);
+            pos
+        }
+    }
+    
 }
 
 
