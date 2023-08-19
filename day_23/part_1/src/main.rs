ac5d4e7fe05068a5bbe0a31cf196814b21e77e41
WillLillis <wlillis@umass.edu>
2023-08-18 23:56:40 -0400

    Day 23 part 1

--- day_23/part_1/src/main.rs
+++ day_23/part_1/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{fs, cmp};
 use std::collections::HashSet;
-use std::ops::Add;
+use std::ops::{Add, AddAssign};
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
 struct XY {
@@ -22,10 +22,19 @@ impl Add for XY {
     }
 }
 
+impl AddAssign for XY {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
+
 #[derive(Debug)]
 struct Elf {
     curr_pos: XY,
-    plan_pos: Option<XY>
+    plan_move: Option<XY>
 }
 
 const ALL_DIRS: [XY; 8] = [XY{x: 1, y: 0}, XY{x: -1, y: 0}, XY{x: 0, y: 1}, 
@@ -52,7 +61,7 @@ fn get_elves(file_name: &str) -> Vec<Elf> {
                 '#' => {
                     elves.push(Elf {
                         curr_pos: XY::new(j as i32, 0i32 - (i as i32)),
-                        plan_pos: None
+                        plan_move: None
                     });
                 },
                 '.' => { continue; },
@@ -68,21 +77,22 @@ fn get_elves(file_name: &str) -> Vec<Elf> {
 
 fn get_next_move(elf: &Elf, curr_elves: &HashSet<XY>, first_check_idx: usize) -> Option<XY> {
     // first check if the elf will move at all
-    let mut collision = false;
+    let mut adjacent = false;
     for dir in ALL_DIRS {
         let tmp_pos = elf.curr_pos + dir;
         if curr_elves.contains(&tmp_pos) {
-            collision = true;
+            adjacent = true;
             break;
         }
     }
 
-    if !collision {
+    if !adjacent {
         return None;
     }
 
     // next we have to check the directions in order
-    for set in CHECK_DIRS.iter().skip(first_check_idx).cycle().take(CHECK_DIRS.len()) {
+    //for set in CHECK_DIRS.iter().skip(first_check_idx).cycle().take(CHECK_DIRS.len()) {
+    for set in CHECK_DIRS.iter().cycle().skip(first_check_idx).take(CHECK_DIRS.len()) {
         if set.iter().fold(true, |accum, dir| accum && !curr_elves.contains(&(elf.curr_pos + *dir))) {
             return Some(set[0]);
         }
@@ -95,10 +105,10 @@ fn get_next_move(elf: &Elf, curr_elves: &HashSet<XY>, first_check_idx: usize) ->
 // update curr elves hashset here??
 fn do_moves(elves: &mut Vec<Elf>, curr_elves: &mut HashSet<XY>) {
     for elf in elves.iter_mut() {
-       match elf.plan_pos {
+       match elf.plan_move {
             Some(pos) => {
                 curr_elves.remove(&elf.curr_pos);
-                elf.curr_pos = pos;
+                elf.curr_pos += pos;
                 curr_elves.insert(elf.curr_pos);
             },
             None => {}
@@ -113,42 +123,51 @@ fn do_moves(elves: &mut Vec<Elf>, curr_elves: &mut HashSet<XY>) {
     // if they're planning on moving and their planned position isn't in the collisions hashset,
     // it's fine to move them
     // otherwise don't move them
-fn set_planned_pos(elves: &mut Vec<Elf>, curr_elves: &HashSet<XY>, first_check_idx: usize) -> u32 {
+fn set_planned_pos(elves: &mut Vec<Elf>, curr_elves: &HashSet<XY>, first_check_idx: usize) -> bool {
     let mut plan_elves: HashSet<XY> = HashSet::new();
     let mut collisions: HashSet<XY> = HashSet::new();
 
     for elf in elves.iter_mut() {
         match get_next_move(elf, curr_elves, first_check_idx) {
-            Some(pos) => {
-                if !plan_elves.insert(pos) {
-                    collisions.insert(pos);
-                    elf.plan_pos = None; // unecessary, will mark as None later
+            Some(dir) => {
+                if !plan_elves.insert(elf.curr_pos + dir) {
+                    collisions.insert(elf.curr_pos + dir);
+                    elf.plan_move = None; // unecessary, will mark as None later
                 } else {
-                    elf.plan_pos = Some(pos);
+                    elf.plan_move = Some(dir);
                 }
             },
             None => {
-                elf.plan_pos = None;
+                elf.plan_move = None;
             }
         }
     }
 
     for elf in elves.iter_mut() {
-        match elf.plan_pos {
-            Some(pos) => {
-                if collisions.contains(&pos) {
-                    elf.plan_pos = None;
+        match elf.plan_move {
+            Some(dir) => {
+                if collisions.contains(&(elf.curr_pos + dir)) {
+                    elf.plan_move = None;
                 }
             },
             None => {}
         }
     }
 
-    return 0;
+    let mut moved = false;
+
+    for elf in elves.iter() {
+        if elf.plan_move != None {
+            moved = true;
+            break;
+        }
+    }
+
+    return moved;
 }
 
 fn main() {
-    let mut elves = get_elves("test_input.txt");
+    let mut elves = get_elves("input.txt");
 
     let mut curr_elves: HashSet<XY> = HashSet::new();
 
@@ -156,10 +175,31 @@ fn main() {
         curr_elves.insert(elf.curr_pos);
     }
     let mut first_check_idx: usize = 0;
-    while set_planned_pos(&mut elves, &curr_elves, first_check_idx) > 0 {
+    let mut rounds_completed: u32 = 0;
+
+    while set_planned_pos(&mut elves, &curr_elves, first_check_idx) {
         first_check_idx += 1 % CHECK_DIRS.len();
         do_moves(&mut elves, &mut curr_elves);
-        println!("hi");
+        rounds_completed += 1;
+        if rounds_completed == 10 {
+            break;
+        }
     }
 
+    let mut min_x = i32::MAX;
+    let mut max_x = i32::MIN;
+    let mut min_y = i32::MAX;
+    let mut max_y = i32::MIN;
+
+    for pos in curr_elves.iter() {
+        min_x = cmp::min(min_x, pos.x);
+        max_x = cmp::max(max_x, pos.x);
+        min_y = cmp::min(min_y, pos.y);
+        max_y = cmp::max(max_y, pos.y);
+    }
+
+    let rect_area = ((max_x - min_x) + 1) * ((max_y - min_y) + 1);
+    let free_spaces = rect_area - curr_elves.len() as i32;
+    
+    println!("Free spaces: {}", free_spaces);
 }
