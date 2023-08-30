5aec33f084be97b10d88156a4ecc7ef32f8c15e7
WillLillis <wlillis@umass.edu>
2023-08-30 16:47:37 -0400

    2021 Day 8 Part 2

--- advent_of_code_2021/day_8/part_2/src/main.rs
+++ advent_of_code_2021/day_8/part_2/src/main.rs
@@ -12,7 +12,7 @@ fn get_vals(file_name: &str) -> Vec<Vec<String>> {
             line.trim()
                 .split_whitespace()
                 .filter(|s| !s.contains('|'))
-                .map(|s| String::from(s))
+                .map(|s| String::from(s.trim()))
                 .collect(),
         );
     }
@@ -21,7 +21,7 @@ fn get_vals(file_name: &str) -> Vec<Vec<String>> {
 }
 
 fn common_with_mapping(input: &str, mapping: &[bool; 7]) -> u32 {
-    return mapping.iter().enumerate().fold(0u32, |accum, (i, flag)| {
+    let common = mapping.iter().enumerate().fold(0u32, |accum, (i, flag)| {
         accum
             + if *flag && input.contains((i as u8 + A_OFFSET) as char) {
                 1
@@ -29,13 +29,32 @@ fn common_with_mapping(input: &str, mapping: &[bool; 7]) -> u32 {
                 0
             }
     });
+
+    return common;
+}
+
+fn decode_segments(input: &str, mappings: &[[bool; 7]; 10]) -> u32 {
+    for (i, mapping) in mappings.iter().enumerate() {
+        if mapping.iter().enumerate().fold(true, |accum, (j, flag)| {
+            accum
+                && if *flag {
+                    input.contains((A_OFFSET + j as u8) as char)
+                } else {
+                    !input.contains((A_OFFSET + j as u8) as char)
+                }
+        }) {
+            return i as u32;
+        }
+    }
+
+    panic!("Failed to decode the current segment!");
 }
 
 fn decode_line(line: &Vec<String>) -> u32 {
-    let mut mappings: [[bool; 7]; 9] = [[false; 7]; 9];
+    let mut mappings: [[bool; 7]; 10] = [[false; 7]; 10];
 
     // get the mappings for the numbers with unique numbers of segments
-    for (i, mapping) in line.iter().take(10).enumerate() {
+    for mapping in line.iter().take(10) {
         match mapping.len() {
             2 => {
                 for segment in mapping.as_bytes() {
@@ -47,12 +66,12 @@ fn decode_line(line: &Vec<String>) -> u32 {
                     mappings[4][(segment - A_OFFSET) as usize] = true;
                 }
             }
-            7 => {
+            3 => {
                 for segment in mapping.as_bytes() {
                     mappings[7][(segment - A_OFFSET) as usize] = true;
                 }
             }
-            8 => {
+            7 => {
                 for segment in mapping.as_bytes() {
                     mappings[8][(segment - A_OFFSET) as usize] = true;
                 }
@@ -63,8 +82,8 @@ fn decode_line(line: &Vec<String>) -> u32 {
         }
     }
 
-    // figure out the rest
-    for (i, mapping) in line.iter().take(10).enumerate() {
+    // figure out the rest of the mappings
+    for mapping in line.iter().take(10) {
         match mapping.len() {
             2 | 3 | 4 | 7 => {
                 continue;
@@ -75,20 +94,61 @@ fn decode_line(line: &Vec<String>) -> u32 {
                     common_with_mapping(mapping, &mappings[4]),
                     common_with_mapping(mapping, &mappings[7]),
                 ) {
-                    (_, _, _) => {}
+                    (6, 3, 3) => {
+                        // 0
+                        for segment in mapping.as_bytes() {
+                            mappings[0][(segment - A_OFFSET) as usize] = true;
+                        }
+                    }
+                    (5, 2, 2) => {
+                        // 2
+                        for segment in mapping.as_bytes() {
+                            mappings[2][(segment - A_OFFSET) as usize] = true;
+                        }
+                    }
+                    (5, 3, 3) => {
+                        // 3
+                        for segment in mapping.as_bytes() {
+                            mappings[3][(segment - A_OFFSET) as usize] = true;
+                        }
+                    }
+                    (5, 3, 2) => {
+                        // 5
+                        for segment in mapping.as_bytes() {
+                            mappings[5][(segment - A_OFFSET) as usize] = true;
+                        }
+                    }
+                    (6, 3, 2) => {
+                        // 6
+                        for segment in mapping.as_bytes() {
+                            mappings[6][(segment - A_OFFSET) as usize] = true;
+                        }
+                    }
+                    (6, 4, 3) => {
+                        // 9
+                        for segment in mapping.as_bytes() {
+                            mappings[9][(segment - A_OFFSET) as usize] = true;
+                        }
+                    }
+                    (_, _, _) => {
+                        panic!("Failed to decipher mappings");
+                    }
                 }
             }
         }
     }
 
-    0
+    let mut place: u32 = 1;
+    return line.iter().skip(10).rev().fold(0, |accum, num| {
+        let tmp = accum + place * decode_segments(num, &mappings);
+        place *= 10;
+        tmp
+    });
 }
 
 /*
  * - Can identify 1, 4, 7, and 8 because of their unique lengths
- * - Can identify the rest by the number of common segments with 1, 4, 7
- *      - Don't need to directly compute number in common with 8, as that's just
- *      the number of segments
+ * - Can identify the rest by the number of common segments with 4 and 7
  *
  *  #|Size|Seg. common w/ 1|Seg. common w/ 4| Seg. common w/ 7|
  *  0|  6 |        2       |        3       |         3       |
@@ -103,9 +163,11 @@ fn decode_line(line: &Vec<String>) -> u32 {
  *  9|  6 |        2       |        4       |         3       |
  */
 fn main() {
-    let entries = get_vals("test_input.txt");
+    let entries = get_vals("input.txt");
 
-    // - each entry contains the mappings for the numbers 0-9
-    // - each sub-entry indicates whether a given letter is used
-    //      - a, b, c, d, e, f, g
+    let sum = entries
+        .iter()
+        .fold(0, |accum, entry| accum + decode_line(entry));
+
+    println!("Sum: {sum}");
 }
