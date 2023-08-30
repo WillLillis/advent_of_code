bd1c64b6a12b34419fb3ea20d8228493836ba8a2
WillLillis <wlillis@umass.edu>
2023-08-30 13:43:59 -0400

    2021 Day 8 part 1

--- advent_of_code_2021/day_8/part_1/src/main.rs
+++ advent_of_code_2021/day_8/part_1/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+
+fn get_output_vals(file_name: &str) -> Vec<Vec<String>> {
+    let input = fs::read_to_string(file_name).expect("Failed to read the input file");
+
+    let mut outputs: Vec<Vec<String>> = Vec::new();
+
+    for line in input.lines() {
+        outputs.push(
+            line.trim()
+                .split_whitespace()
+                .skip_while(|s| !s.contains('|'))
+                .skip(1)
+                .map(|s| String::from(s))
+                .collect(),
+        );
+    }
+
+    return outputs;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let num_occurences = get_output_vals("input.txt")
+        .iter()
+        .flatten()
+        .fold(0, |accum, s| {
+            accum
+                + if [2, 3, 4, 7].contains(&s.len()) {
+                    1
+                } else {
+                    0
+                }
+        });
+
+    println!("Num occurences: {num_occurences}");
 }
