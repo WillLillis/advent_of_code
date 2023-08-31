0b6ca11673c4b0984d31575c67829f120e3d3cf1
WillLillis <wlillis@umass.edu>
2023-08-31 16:34:52 -0400

    2021 Day 9 Part 2

--- advent_of_code_2021/day_9/part_2/src/main.rs
+++ advent_of_code_2021/day_9/part_2/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs;
 
 fn get_height_map(file_name: &str) -> Vec<Vec<u32>> {
@@ -36,12 +37,64 @@ fn is_lowest(row: usize, col: usize, map: &Vec<Vec<u32>>) -> bool {
 }
 
 fn get_basin_size(row: usize, col: usize, map: &Vec<Vec<u32>>) -> u32 {
-    
-    0
+    // not sure what the efficient way is to do this part, but this will work
+    let mut basin: HashSet<(usize, usize)> = HashSet::new();
+    basin.insert((row, col));
+
+    let mut to_insert: Vec<(usize, usize)> = Vec::new();
+    loop {
+        to_insert.clear();
+        for &(row, col) in basin.iter() {
+            let val = map[row][col];
+
+            // check up if we can
+            if row != 0
+                && map[row - 1][col] > val
+                && map[row - 1][col] != 9
+                && !basin.contains(&(row - 1, col))
+            {
+                to_insert.push((row - 1, col));
+            }
+            // check down if we can
+            if (row < map.len() - 1)
+                && map[row + 1][col] > val
+                && map[row + 1][col] != 9
+                && !basin.contains(&(row + 1, col))
+            {
+                to_insert.push((row + 1, col));
+            }
+            // check left if we can
+            if col != 0
+                && map[row][col - 1] > val
+                && map[row][col - 1] != 9
+                && !basin.contains(&(row, col - 1))
+            {
+                to_insert.push((row, col - 1));
+            }
+            // check right
+            if (col < map[0].len() - 1)
+                && map[row][col + 1] > val
+                && map[row][col + 1] != 9
+                && !basin.contains(&(row, col + 1))
+            {
+                to_insert.push((row, col + 1));
+            }
+        }
+
+        if to_insert.is_empty() {
+            break;
+        } else {
+            for &(row, col) in to_insert.iter() {
+                basin.insert((row, col));
+            }
+        }
+    }
+
+    return basin.len() as u32;
 }
 
 fn main() {
-    let map = get_height_map("test_input.txt");
+    let map = get_height_map("input.txt");
     // find basins
     // find corresponding basin sizes
     // only keep the top three
@@ -52,7 +105,7 @@ fn main() {
         for j in 0..map[0].len() {
             if is_lowest(i, j, &map) {
                 basin_sizes[3] = get_basin_size(i, j, &map);
-                basin_sizes.sort();
+                basin_sizes.sort_by(|a, b| b.cmp(a));
             }
         }
     }
