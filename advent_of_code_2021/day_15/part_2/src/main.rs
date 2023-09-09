aaba31eddfccd4a204373064f74386cafb3796f6
WillLillis <wlillis@umass.edu>
2023-09-09 16:03:26 -0400

    2021 Day 15 part 2 running very slow, not sure if I made a mistake in implementing Dijkstra's alg

--- advent_of_code_2021/day_15/part_2/src/main.rs
+++ advent_of_code_2021/day_15/part_2/src/main.rs
@@ -7,45 +7,43 @@ fn get_map(file_name: &str) -> Vec<Vec<u8>> {
     let mut map: Vec<Vec<u8>> = Vec::new();
 
     for line in input.lines() {
-        map.push(
-            line.chars()
-                .filter_map(|c| c.to_digit(10))
-                .map(|x| x as u8)
-                .collect(),
-        );
+        let mut row: Vec<u8> = line
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .map(|x| x as u8)
+            .collect();
+        let mut accum: Vec<u8> = Vec::new();
+        for i in 1..5 {
+            let mut tmp = row.clone();
+            tmp.iter_mut().for_each(|x| {
+                if *x + i <= 9 {
+                    *x += i
+                } else {
+                    *x = (*x + i) % 9
+                }
+            });
+            accum.append(&mut tmp);
+        }
+        row.append(&mut accum);
+        map.push(row);
     }
+
+    let mut accum: Vec<Vec<u8>> = Vec::new();
+    for i in 1..5 {
+        let mut tmp = map.clone();
+        tmp.iter_mut().flatten().for_each(|x| {
+            if *x + i <= 9 {
+                *x += i
+            } else {
+                *x = (*x + i) % 9
+            }
+        });
+        accum.append(&mut tmp);
+    }
+    
+    map.append(&mut accum);
 
     return map;
-}
-
-fn get_neighbors(
-    i: usize,
-    j: usize,
-    map_len: usize,
-    map_width: usize,
-    neigh_arr: &mut [Option<(usize, usize)>; 4],
-) {
-    neigh_arr.iter_mut().for_each(|x| *x = None);
-    let mut idx = 0usize;
-    // check up
-    if i > 0 {
-        neigh_arr[idx] = Some((i - 1, j));
-        idx += 1;
-    }
-    // check down
-    if i < map_len - 1 {
-        neigh_arr[idx] = Some((i + 1, j));
-        idx += 1;
-    }
-    // check left
-    if j > 0 {
-        neigh_arr[idx] = Some((i, j - 1));
-        idx += 1;
-    }
-    // check right
-    if j < map_width - 1 {
-        neigh_arr[idx] = Some((i, j + 1));
-    }
 }
 
 // easy application of Djikstra...
@@ -88,8 +86,9 @@ fn safest_path_risk(map: &Vec<Vec<u8>>) -> u32 {
                 if j < map_width - 1 {
                     neighbors[idx] = Some((i, j + 1));
                 }
-                for &neigh in neighbors.iter() { // could just go off of idx, but it shouldn't make
-                                                 // much of a difference
+                for &neigh in neighbors.iter() {
+                    // could just go off of idx, but it shouldn't make
+                    // much of a difference
                     match neigh {
                         Some((a, b)) => {
                             let alt_dist = dist[i][j]
@@ -121,6 +120,10 @@ fn safest_path_risk(map: &Vec<Vec<u8>>) -> u32 {
 
 fn main() {
     let map = get_map("input.txt");
+
+    for row in map.iter() {
+        println!("{:?}", row);
+    }
 
     let lowest_risk = safest_path_risk(&map);
 
