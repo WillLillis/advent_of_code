fe46e7fe9aca7266ac5f1e5cc0f8fc1fcd7a9ea9
WillLillis <wlillis@umass.edu>
2023-09-19 18:28:22 -0400

    2021 Day 17 Part 1, meh solution

--- advent_of_code_2021/day_17/part_1/src/main.rs
+++ advent_of_code_2021/day_17/part_1/src/main.rs
@@ -1,15 +1,76 @@
-use std::{fs, process};
+use std::cmp::{self, Ordering};
+use std::{fs, ops::AddAssign, process};
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct XY {
     x: i32,
     y: i32,
 }
 
+impl XY {
+    fn in_area(&self, area: &Area) -> bool {
+        if !(self.x >= area.bottom_left.x && self.x <= area.bottom_right.x) {
+            return false;
+        }
+
+        if !(self.y >= area.bottom_left.y && self.y <= area.top_left.y) {
+            return false;
+        }
+
+        return true;
+    }
+    // how to tell when we've passed by the target area
+    // going to make some restrictive assumptions
+    //      - since the problem wants the probe to be shot
+    //      as high as possible, we'll assume we're approaching
+    //      from above
+    //      - since we start at (0, 0), we can tell if we're approaching
+    //      from the left or right
+    //          - some issues if the target area "straddles" but we'll
+    //          ignore that for now
+    fn area_unreachable(&self, area: &Area) -> bool {
+        if self.y < area.bottom_left.y {
+            return true;
+        }
+        // approaching from left to right
+        if area.bottom_left.x > 0 {
+            if self.x > area.bottom_right.x {
+                return true;
+            }
+        } else if area.bottom_right.x < 0 {
+            // approaching right to left
+            if self.x < area.bottom_left.x {
+                return true;
+            }
+        } else {
+            panic!("Unexplored target area case! Target area straddles x=0");
+        }
+
+        return false;
+    }
+}
+
+impl AddAssign for XY {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
+
+#[derive(Debug)]
+struct Area {
+    bottom_left: XY,
+    top_left: XY,
+    bottom_right: XY,
+    top_right: XY,
+}
+
 // first get the basic sim step working
 // then work out a way to efficiently search the possible v_0 vectors
 
-fn get_target_area(file_name: &str) -> (XY, XY, XY, XY) {
+fn get_target_area(file_name: &str) -> Area {
     let input = fs::read_to_string(file_name).unwrap_or_else(|err| {
         eprintln!("Error occurred while opening file 1: {err}");
         process::exit(1);
@@ -39,11 +100,88 @@ fn get_target_area(file_name: &str) -> (XY, XY, XY, XY) {
         y: vals[3],
     };
 
-    return (bottom_left, top_left, bottom_right, top_right);
+    return Area {
+        bottom_left,
+        top_left,
+        bottom_right,
+        top_right,
+    };
+}
+
+fn sim_step(pos: &mut XY, velocity: &mut XY) {
+    *pos += *velocity;
+    match velocity.x.cmp(&0) {
+        Ordering::Less => {
+            velocity.x += 1;
+        }
+        Ordering::Equal => {}
+        Ordering::Greater => {
+            velocity.x -= 1;
+        }
+    }
+    velocity.y -= 1;
+}
+
+// returns Some(max_height) if target area is reached,
+// None if target area is missed
+fn sim_shot(v0: XY, target: &Area) -> Option<i32> {
+    let mut max_ht = 0;
+    let mut pos = XY { x: 0, y: 0 };
+    let mut velocity = v0;
+
+    loop {
+        if pos.in_area(target) {
+            return Some(max_ht);
+        }
+        if pos.area_unreachable(target) {
+            return None;
+        }
+        sim_step(&mut pos, &mut velocity);
+
+        max_ht = cmp::max(max_ht, pos.y)
+    }
 }
 
 fn main() {
-    let corners = get_target_area("test_input.txt");
+    let area = get_target_area("input.txt");
 
-    println!("{:?}", corners);
+    let init_x_velo = if area.bottom_left.x > 0 { 1 } else { -1 };
+
+    let mut max_ht = 0;
+
+    if init_x_velo > 0 {
+        for x_velo in init_x_velo..area.bottom_right.x {
+            // what's a reasonable stopping condition for y velo?
+            for y_velo in 1..1000 {
+                let v0 = XY {
+                    x: x_velo,
+                    y: y_velo,
+                };
+                match sim_shot(v0, &area) {
+                    Some(ht) => {
+                        max_ht = cmp::max(max_ht, ht);
+                    }
+                    None => {}
+                }
+            }
+        }
+    } else {
+        for x_velo in (init_x_velo..area.bottom_left.x).rev() {
+            // what's a reasonable stopping condition for y velo?
+            for y_velo in 1..1000 {
+                let v0 = XY {
+                    x: x_velo,
+                    y: y_velo,
+                };
+                match sim_shot(v0, &area) {
+                    Some(ht) => {
+                        max_ht = cmp::max(max_ht, ht);
+                    }
+                    None => {}
+                }
+            }
+        }
+    }
+
+    println!("Max height: {max_ht}");
 }
