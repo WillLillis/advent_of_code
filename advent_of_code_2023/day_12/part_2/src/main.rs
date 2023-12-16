e6de4a3df35b0e29dba48b902f28ecf457be8583
WillLillis <wlillis@umass.edu>
2023-12-16 17:03:10 -0500

    2023 Day 12 Part 2, giving up for now

--- advent_of_code_2023/day_12/part_2/src/main.rs
+++ advent_of_code_2023/day_12/part_2/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use itertools::Itertools;
 use memoize::memoize;
 use rayon::prelude::*;
@@ -16,7 +18,9 @@ enum SpringCondition {
 struct SpringRecord {
     id: usize,
     springs: Vec<SpringCondition>,
+    alt_springs: VecDeque<SpringCondition>,
     contig_sizes: Vec<u32>,
+    alt_contig_sizes: VecDeque<u32>,
 }
 
 fn get_records(input: &str) -> Vec<SpringRecord> {
@@ -47,8 +51,10 @@ fn get_records(input: &str) -> Vec<SpringRecord> {
 
         records.push(SpringRecord {
             id,
-            springs,
-            contig_sizes,
+            springs: springs.clone(),
+            alt_springs: springs.into(),
+            contig_sizes: contig_sizes.clone(),
+            alt_contig_sizes: contig_sizes.into(),
         });
 
         id += 1;
@@ -151,7 +157,108 @@ fn recur(record: &mut SpringRecord) -> usize {
     return damaged_count + op_count;
 }
 
-#[memoize(SharedCache)]
+#[memoize]
+fn recursion_again(springs: VecDeque<SpringCondition>, sizes: VecDeque<u32>) -> usize {
+    // check for base case
+    match (springs.is_empty(), sizes.is_empty()) {
+        (true, true) => {
+            return 1;
+        }
+        (true, false) => {
+            return 0;
+        }
+        _ => {}
+    }
+
+    let mut springs = springs.clone();
+    match springs.front() {
+        Some(SpringCondition::Operational) => {
+            springs.pop_front();
+            return recursion_again(springs, sizes);
+        }
+        Some(SpringCondition::Unknown) => {
+            // Try swapping the unknown node with an operational node, and then a damaged one
+
+            // Operational first
+            springs[0] = SpringCondition::Operational;
+            let mut count = recursion_again(springs.clone(), sizes.clone());
+
+            // Now for damaged. make sure doing so could lead to a valid config
+            if !sizes.is_empty() && springs.len() >= sizes[0] as usize {
+                // ensure the next group size is "doable"
+                springs[0] = SpringCondition::Damaged;
+                if !springs
+                    .iter()
+                    .take(sizes[0] as usize)
+                    .contains(&SpringCondition::Operational)
+                {
+                    // mark the next group as damaged
+                    for i in 0..sizes[0] as usize {
+                        springs[i] = SpringCondition::Damaged;
+                    }
+                    // check one past to avoid an edge case
+                    if springs.len() > sizes[0] as usize {
+                        match springs[sizes[0] as usize] {
+                            SpringCondition::Damaged => {
+                                // dead end
+                            }
+                            SpringCondition::Operational => {
+                                // ok
+                                count += recursion_again(springs.clone(), sizes.clone());
+                            }
+                            SpringCondition::Unknown => {
+                                // mark as operational
+                                springs[sizes[0] as usize] = SpringCondition::Operational;
+                                count += recursion_again(springs.clone(), sizes.clone());
+                            }
+                        }
+                    } else {
+                        // otherwise if there's nothing past our group, we're find to just recurse
+                        count += recursion_again(springs.clone(), sizes.clone());
+                    }
+                }
+            }
+            return count;
+        }
+        Some(SpringCondition::Damaged) => {
+            // make sure we can make the next group
+            if sizes.is_empty() {
+                return 0;
+            }
+            let mut sizes = sizes.clone();
+            let group_size = sizes.pop_front().unwrap();
+            if springs.len() < group_size as usize {
+                return 0;
+            }
+
+            let group = springs.drain(0..group_size as usize);
+            if group.into_iter().contains(&SpringCondition::Operational) {
+                return 0;
+            }
+            // check one past our group, similar edge case to the SpringCondition::Unknown case
+            if !springs.is_empty() {
+                match springs[0] {
+                    SpringCondition::Operational => {}
+                    SpringCondition::Unknown => {
+                        springs[0] = SpringCondition::Operational;
+                    }
+                    SpringCondition::Damaged => {
+                        return 0;
+                    }
+                }
+            }
+
+            return recursion_again(springs.clone(), sizes.clone());
+        }
+        None => {
+            unreachable!()
+        }
+    }
+}
+
+// using SharedCache option leads to a LOT of kernel time for each CPU thread,
+// seems to slow everything down...
+#[memoize(Ignore: id, Capacity: 1000000)]
 fn recur_memo2(springs: Vec<SpringCondition>, id: usize) -> usize {
     let mut springs = springs.clone();
     if !springs.contains(&SpringCondition::Unknown) {
@@ -190,16 +297,29 @@ fn main() {
 
     let sum: usize = records
         //.par_iter_mut()
-        .par_iter()
+        //.par_iter()
+        .iter()
+        //.iter_mut()
         .enumerate()
         .map(|(i, r)| {
             //.map(|(i, mut r)| {
             //let count = recur(&mut r);
-            let count = recur_memo2(r.springs.clone(), r.id);
+            //let count = recur_memo2(r.springs.clone(), r.id);
+            println!("{:?}", r.alt_springs);
+            let count = recursion_again(r.alt_springs.clone(), r.alt_contig_sizes.clone());
             println!("{i}: {count}");
             count
         })
         .sum();
-
     println!("Sum: {}", sum);
+
+    /*println!("Test: {:#?}", records[2]);
+    println!(
+        "Combinations: {}",
+        recursion_again(
+            records[2].alt_springs.clone(),
+            records[2].alt_contig_sizes.clone(),
+            0
+        )
+    );*/
 }
